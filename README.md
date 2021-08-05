5f3c19c9dc48f4d7234d52d1178355535d5cfa4a
yango3 <o3.yang@hotmail.com>
2021-08-05 10:58:41 +0800

    Add spring lib

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dianke-raspberry-java
 
-### Run as a jar file
+### Run as a jar file on Raspberry Pi 4
 ```shell
 /usr/bin/java -Dfile.encoding=UTF-8 -jar  /home/pi/Documents/project/dianke-raspberry-java/out/artifacts/packjava_jar/packjava.jar
 
--- src/main/java/com/yango/packj/PackJ.java
+++ src/main/java/com/yango/packj/PackJ.java
@@ -1,8 +1,24 @@
 package com.yango.packj;
 
+import com.yango.packj.client.InsertRecord;
+import com.yango.packj.client.PosClient;
+
 public class PackJ {
+    static String privateKey = "cdc7cc95755f19aa8168e2b0c3dd89d556be87b60608835549c0aee38d156640";
+    static String contractAddress = "0x7c567f388fbe437e5707b3556ec911d0230b9d01";
+
     public static void main(String[] args) {
         System.out.println("Hello pack java.");
         System.out.println("To do the real work now.");
+
+        PosClient posClient = new PosClient(1, privateKey);
+
+        int rtn = posClient.getIndex(contractAddress);
+
+        System.out.println("index is: " + rtn);
+
+        // insert a pIn record
+
+        posClient.stop();
     }
 }
